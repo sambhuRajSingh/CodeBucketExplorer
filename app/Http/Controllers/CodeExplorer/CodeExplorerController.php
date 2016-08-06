75117ef909c59fcc798956659b582b248f01b55a
sambhuka <sambhu@netsend.com>
2016-08-06 17:06:50 +0100

    Initial Git API response Check

--- app/Http/Controllers/CodeExplorer/CodeExplorerController.php
+++ app/Http/Controllers/CodeExplorer/CodeExplorerController.php
@@ -5,6 +5,7 @@ namespace CodeExplorer\Http\Controllers\CodeExplorer;
 use Illuminate\Http\Request;
 
 use CodeExplorer\Http\Requests;
+use GuzzleHttp\Client as GuzzleHttpClient;
 use CodeExplorer\Http\Controllers\Controller;
 
 class CodeExplorerController extends Controller
@@ -16,6 +17,23 @@ class CodeExplorerController extends Controller
      */
     public function index()
     {
+        try {
+            $client = new GuzzleHttpClient();
+            // $apiUrl = "https://api.github.com/repos/nicolas-grekas/symfony/commits/master?per_page=10";
+            $apiUrl = "https://api.github.com/repos/nicolas-grekas/symfony/commits?per_page=2";
+
+            $apiRequest = $client->request('GET', $apiUrl,
+                [
+                    // 'per_page' => 10
+                    //'auth' => ['sambhuRajSingh', '*****']
+                ]
+            );
+
+            dd(collect(json_decode($apiRequest->getBody()->getContents())));
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+
         return view('welcome');
     }
 }
