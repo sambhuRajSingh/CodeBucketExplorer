d93c2bf53b81133657f07ae8b30ec70a0ddfa518
sambhuka <sambhu@netsend.com>
2016-08-07 15:01:08 +0100

    Added title, Links on Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,40 +5,49 @@
         <div class="col-xs-12">
             @include('partials.alerts.notify')
 
-            <table class="table">
+            <table class="table table-striped" >
                 <tbody>
                     @foreach($commits as $commit)
-                        <tr class="hidden-xs">
-                            <td>
-                                <img src="{{ Gravatar::src($commit->commit->author->email, 45) }}">
-                            </td>
-                            <td>
-                                <p class="lead">{{ $commit->commit->message }}</p>
-                                {{ $commit->sha }}
-                            </td>
-                            <td>
-                                <em>by</em> {{ $commit->commit->author->name }}
-                                <br>
-                                <small><em>on</em> {{ $commit->commit->author->date }}</small>
-                            </td>
-                        </tr>
-
-                        <tr class="visible-xs">
+                        <tr>
                             <td width="85">
-                                <img src="{{ Gravatar::src($commit->commit->author->email, 80) }}">
+                                <a href="{{ $commit->author->html_url }}"
+                                   title="View {{ $commit->author->login }} profile on Git Hub Website.">
+                                    <img src="{{ Gravatar::src($commit->commit->author->email, 79) }}">
+                                </a>
                             </td>
-                            <td>
+                            <td class="hidden-xs"><!-- hidden on Mobile -->
+                                <strong>{{ str_limit($commit->commit->message, 150) }}</strong>
+                                <br>
+                                <a href="{{ $commit->html_url }}"
+                                   title="View the Commit on the Git Hub Website">
+                                    {{ str_limit($commit->sha, 10) }}
+                                </a>
+                            </td>
+                            <td class="visible-xs"><!-- For Mobile View -->
                                 <strong>{{ str_limit($commit->commit->message, 30) }}</strong>
                                 <br>
-                                <a href="https://github.com/{{ $repositoryOwner }}/{{ $repositoryName }}/commit/{{ $commit->sha }}"
+                                <a href="{{ $commit->html_url }}"
                                    title="View the Commit on the Git Hub Website">
                                     {{ str_limit($commit->sha, 10) }}
                                 </a>
 
                                 <br><br>
 
-                                <em>by</em> {{ $commit->commit->author->name }}
+                                <em>by</em>
+                                <a href="{{ $commit->author->html_url }}"
+                                   title="View {{ $commit->author->login }} profile on Git Hub Website.">
+                                   {{ $commit->commit->author->name }}
+                                </a>
                                 -
+                                <small><em>{{ \Carbon\Carbon::parse($commit->commit->author->date)->diffForHumans() }}</em></small>
+                            </td>
+                            <td class="hidden-xs" width="150px">
+                                <em>by</em>
+                                <a href="{{ $commit->author->html_url }}"
+                                   title="View {{ $commit->author->login }} profile on Git Hub Website.">
+                                   {{ $commit->commit->author->name }}
+                                </a>
+                                <br>
                                 <small><em>{{ \Carbon\Carbon::parse($commit->commit->author->date)->diffForHumans() }}</em></small>
                             </td>
                         </tr>
